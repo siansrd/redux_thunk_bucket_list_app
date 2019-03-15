0912ca7d416e5162121490a8001b78f319886e85
siansrd <sianrobinsondavies@gmail.com>
2019-03-15 13:59:52 +0000

    Configure redux dev tools

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers/index.js';
 
-const store = createStore(reducer, {})
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(reducer, {}, composeEnhancers())
 
 ReactDOM.render(
   <Provider store={ store }>
