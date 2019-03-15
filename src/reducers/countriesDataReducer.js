134618202a8e58f6c60d84e3519b5e4f050aa751
siansrd <sianrobinsondavies@gmail.com>
2019-03-15 15:12:56 +0000

    Reshapes state's data structure

--- src/reducers/countriesDataReducer.js
+++ src/reducers/countriesDataReducer.js
@@ -1,11 +1,10 @@
 const CountriesDataReducer = (state = [], action) => {
   switch(action.type){
     case 'GET_COUNTRIES_DATA':
-      return [...state, action.countryData]
+      return [...state, ...action.countryData]
     default:
       return state;
   }
-  return state;
 }
 
 export default CountriesDataReducer;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,5 +3,5 @@ import { combineReducers } from 'redux';
 import countriesDataReducer from './countriesDataReducer.js';
 
 export default combineReducers({
-  countriesDataReducer
+  countriesData: countriesDataReducer
 });
