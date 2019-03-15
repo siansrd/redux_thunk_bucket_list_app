24854b680615341ccba33add6b7791a5f6d85cf7
siansrd <sianrobinsondavies@gmail.com>
2019-03-15 17:45:13 +0000

    Add action for change of 'visited' status

--- src/actions/bucketListActions.js
+++ src/actions/bucketListActions.js
@@ -2,3 +2,8 @@ export const addToBucketList = (country) => ({
     type: 'ADD_COUNTRY',
     country
 })
+
+export const toggleVisitedStatus = (countryName) => ({
+    type: 'TOGGLE_VISITED_STATUS',
+    countryName
+})
