9e7cbdc4d1ae285fdc19fe807074f70a0720b080
siansrd <sianrobinsondavies@gmail.com>
2019-03-15 15:30:32 +0000

    Add bucket list reducer

---
+++ src/reducers/bucketListReducer.js
@@ -0,0 +1,5 @@
+const bucketListReducer = (state = [], action) => {
+  return state
+}
+
+export default bucketListReducer;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import countriesDataReducer from './countriesDataReducer.js';
+import bucketListReducer from './bucketListReducer.js';
 
 export default combineReducers({
-  countriesData: countriesDataReducer
+  countriesData: countriesDataReducer,
+  bucketList: bucketListReducer
 });
