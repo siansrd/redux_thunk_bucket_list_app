639321b045fca019a82d0535e5a989e71601f703
siansrd <sianrobinsondavies@gmail.com>
2019-03-15 19:37:49 +0000

    Refactor to handle checkbox change in Bucklet list

--- src/components/BucketList.js
+++ src/components/BucketList.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import BucketListItem from './BucketListItem.js';
+import { toggleVisitedStatus } from '../actions/bucketListActions';
 
 class BucketList extends Component {
 
   constructor() {
     super();
     this.createListItems = this.createListItems.bind(this);
+    this.visitedStatusChange = this.visitedStatusChange.bind(this);
   }
 
   createListItems() {
     return this.props.bucketList.map(country => {
-      return <BucketListItem country={ country } />
+      return <BucketListItem country={ country } onCheck={ this.visitedStatusChange } />
     })
+  }
+
+  visitedStatusChange(evt) {
+    this.props.visitedStatusChange(evt.target.id);
   }
 
   render() {
@@ -21,10 +27,16 @@ class BucketList extends Component {
 
 }
 
+const mapDispatchToProps = dispatch => ({
+  visitedStatusChange(countryName) {
+    dispatch(toggleVisitedStatus(countryName))
+  }
+})
+
 const mapStateToProps = (state) => {
   return {
     bucketList: state.bucketList
   }
 }
 
-export default connect(mapStateToProps)(BucketList);
+export default connect(mapStateToProps, mapDispatchToProps)(BucketList);
--- src/components/BucketListItem.js
+++ src/components/BucketListItem.js
@@ -1,35 +1,19 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { toggleVisitedStatus } from '../actions/bucketListActions';
+import React from 'react';
 
-class BucketListItem extends Component {
+const BucketListItem = (props) => {
+  const { name, visited } = props.country;
 
-  visitedStatusChanged(evt) {
-    const name = evt.target.id;
-    this.props.visitedStatusChanged(name);
-  }
-
-  render() {
-    const { name, visited } = this.props.country
-
-    return (
-      <li>
-        <p>{ name }</p>
-        <input 
-          type="checkbox" 
-          checked={ visited }
-          id={ name }
-          onChange={ evt => this.visitedStatusChanged(evt) } 
-        />
-      </li>
-    )
-  }
+  return (
+    <li>
+      <p>{ name }</p>
+      <input 
+        type="checkbox" 
+        onChange={ props.onCheck }
+        checked={ visited }
+        id={ name } 
+      />
+    </li>
+  )
 }
 
-const mapDispatchToProps = dispatch => ({
-  visitedStatusChanged(countryName) {
-    dispatch(toggleVisitedStatus(countryName))
-  }
-})
-
-export default connect(null, mapDispatchToProps)(BucketListItem)
+export default BucketListItem;
