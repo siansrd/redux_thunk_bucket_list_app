2339c0d18f581067c66ddf4bc2a747c2f8ff11d1
siansrd <sianrobinsondavies@gmail.com>
2019-04-21 12:53:06 +0100

    Refactor class into functional component

--- src/components/BucketList.js
+++ src/components/BucketList.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import BucketListItem from './BucketListItem.js';
 import { toggleVisitedStatus } from '../actions/bucketListActions';
 import './BucketList.css';
 
-class BucketList extends Component {
+const BucketList = (props) => {
 
-  createListItems() {
-    return this.props.listItems.map(country => {
+  const createListItems = () => {
+    return props.listItems.map(country => {
       return <BucketListItem 
         country={ country } 
-        onCheck={ (evt) => {this.props.visitedStatusChange(evt.target.id)} } />
+        onCheck={ (evt) => {props.visitedStatusChange(evt.target.id)} } />
     })
   }
 
-  render() {
-    return this.props.listItems.length === 0 ? 
-      <h5>Nothing in your list</h5> : 
-      <ul className="bucket-list">{ this.createListItems() }</ul>
-  }
+  return props.listItems.length === 0 ? 
+    <h5>Nothing in your list</h5> : 
+    <ul className="bucket-list">{ createListItems() }</ul>
 
 }
 
