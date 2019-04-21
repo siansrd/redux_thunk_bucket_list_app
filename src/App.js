92966acdccb9c62bbcc6a9f419799b3fec5ea1a7
siansrd <sianrobinsondavies@gmail.com>
2019-04-21 12:34:14 +0100

    Move API request into App to delete CountriesData

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import CountriesData from './components/CountriesData.js';
+import { getCountriesData } from './actions/countriesDataActions';
 import BucketLists from './components/BucketLists.js';
+import Select from './components/Select';
+import { connect } from 'react-redux';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.onPageLoad()
+  }
+
   render() {
     return (
       <>
         <h1>Bucket List</h1>
         <h3>All the places you want to go before you die</h3>
-        <CountriesData />
+        <Select />
         <BucketLists />
       </>
     );
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  onPageLoad() {
+    dispatch(getCountriesData())
+  }
+})
+
+export default connect(null, mapDispatchToProps)(App);
