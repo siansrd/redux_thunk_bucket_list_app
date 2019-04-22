e15744b7bcfd1221cdde5e306f931ce5c35ee421
siansrd <sianrobinsondavies@gmail.com>
2019-04-22 13:22:32 +0100

    Fix syntax for consistency

--- src/reducers/bucketListReducer.js
+++ src/reducers/bucketListReducer.js
@@ -1,15 +1,15 @@
 const bucketListReducer = (state = [], action) => {
-  switch(action.type){
+  switch (action.type) {
     case 'ADD_COUNTRY':
-      return [...state, action.country]
+      return [...state, action.country];
     case 'TOGGLE_VISITED_STATUS':
-      return state.map(item => {
+      return state.map((item) => {
         if (action.countryName === item.name) item.visited = !item.visited;
         return item;
-      })
+      });
     default:
       return state;
   }
-}
+};
 
 export default bucketListReducer;
