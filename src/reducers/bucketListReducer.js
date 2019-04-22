4cb13d88d500692f771bfc93540b46921286f247
siansrd <sianrobinsondavies@gmail.com>
2019-04-22 13:37:27 +0100

    Prevent duplicate being added to bucketlist

--- src/reducers/bucketListReducer.js
+++ src/reducers/bucketListReducer.js
@@ -1,6 +1,7 @@
 const bucketListReducer = (state = [], action) => {
   switch (action.type) {
     case 'ADD_COUNTRY':
+      if (state.find(item => item.name === action.country.name)) return state;
       return [...state, action.country];
     case 'TOGGLE_VISITED_STATUS':
       return state.map((item) => {
