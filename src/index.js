53746e6999cf3ae5cc7ed6c3167f6c0aa536dba8
siansrd <sianrobinsondavies@gmail.com>
2019-04-24 19:32:11 +0100

    Fix syntax for consistency

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App.jsx';
-import * as serviceWorker from './serviceWorker';
-
-import { createStore, compose, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import reducer from './reducers/index.js';
 import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import * as serviceWorker from './serviceWorker';
+import './index.css';
+import App from './App';
+import reducer from './reducers/index';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, {}, composeEnhancers(applyMiddleware(thunk)))
+const store = createStore(reducer, {}, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <Provider store={ store }>
+  <Provider store={store}>
     <App />
-  </ Provider>, 
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
